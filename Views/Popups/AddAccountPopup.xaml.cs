9aef212269a9e3f9d37ecb39db29de514f2b57d3
Ken Murao <murao.ken.36x@kyoto-u.jp>
2024-12-25 01:18:12 +0900

    Update to be able to register with the editing non-confirmed values

--- Views/Popups/AddAccountPopup.xaml.cs
+++ Views/Popups/AddAccountPopup.xaml.cs
@@ -1,5 +1,6 @@
 using CommunityToolkit.Maui.Views;
 using KakeiboApp.ViewModels.Popups;
+using Microsoft.Maui.Handlers;
 
 namespace KakeiboApp.Views.Popups;
 
@@ -10,5 +11,25 @@ public partial class AddAccountPopup : Popup
 		InitializeComponent();
         vm.popup = this;
         BindingContext = vm;
+
+        FixButtonFocusingBehavior();
     }
+
+    private void FixButtonFocusingBehavior()
+    {
+        //https://github.com/dotnet/maui/issues/23901#issuecomment-2429507942
+        ButtonHandler.Mapper.AppendToMapping("UnfocusFix", (h, b) =>
+        {
+            h.PlatformView.FocusableInTouchMode = true;
+        });
+
+        ButtonHandler.Mapper.PrependToMapping(nameof(Button.IsFocused), (h, b) =>
+        {
+            if (b.IsFocused)
+            {
+                b.Clicked();
+            }
+        });
+    }
+
 }
