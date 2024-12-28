2ea2d067f19e9592bf0e9ca90a718fcb62846b35
Ken Murao <murao.ken.36x@kyoto-u.jp>
2024-12-28 12:02:33 +0900

    minor fix

--- ViewModels/RegisterPageViewModel.cs
+++ ViewModels/RegisterPageViewModel.cs
@@ -36,21 +36,21 @@ public partial class RegisterPageViewModel : BaseViewModel
     private void InitializeFormData()
     {
         var spendingItem = DataForm.DataObject as SpendingItem;
-        spendingItem.GetType().GetProperty(nameof(SpendingItem.Date))?.SetValue(spendingItem, DateTime.Today);
+        spendingItem?.GetType().GetProperty(nameof(SpendingItem.Date))?.SetValue(spendingItem, DateTime.Today);
         DataForm.UpdateEditor(nameof(SpendingItem.Date));
-        spendingItem.GetType().GetProperty(nameof(SpendingItem.Category))?.SetValue(spendingItem, Categories.First());
+        spendingItem?.GetType().GetProperty(nameof(SpendingItem.Category))?.SetValue(spendingItem, Categories.First());
         DataForm.UpdateEditor(nameof(SpendingItem.Category));
-        spendingItem.GetType().GetProperty(nameof(SpendingItem.Name))?.SetValue(spendingItem, string.Empty);
+        spendingItem?.GetType().GetProperty(nameof(SpendingItem.Name))?.SetValue(spendingItem, string.Empty);
         DataForm.UpdateEditor(nameof(SpendingItem.Name));
         decimal defaultAmount = 0;
-        spendingItem.GetType().GetProperty(nameof(SpendingItem.Amount))?.SetValue(spendingItem, defaultAmount);
+        spendingItem?.GetType().GetProperty(nameof(SpendingItem.Amount))?.SetValue(spendingItem, defaultAmount);
         DataForm.UpdateEditor(nameof(SpendingItem.Amount));
-        spendingItem.GetType().GetProperty(nameof(SpendingItem.Note))?.SetValue(spendingItem, string.Empty);
+        spendingItem?.GetType().GetProperty(nameof(SpendingItem.Note))?.SetValue(spendingItem, string.Empty);
         DataForm.UpdateEditor(nameof(SpendingItem.Note));
     }
 
     [RelayCommand]
-    public async Task RegisterItemAsync(SfDataForm dataForm)
+    public async Task RegisterItemAsync()
     {
         if (IsBusy)
             return;
