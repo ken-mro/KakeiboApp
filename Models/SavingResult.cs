9ff9194084797e7ecbbb612b144d3a9d4f4784bd
Ken Murao <murao.ken.36x@kyoto-u.jp>
2024-12-31 23:11:11 +0900

    Update SavingResult class and show sum of the balances

--- Models/SavingResult.cs
+++ Models/SavingResult.cs
@@ -3,5 +3,40 @@
 public class SavingResult
 {
     public string Name { get; set; } = string.Empty;
-    public decimal Amount { get; set; }
+    public decimal Amount { get; init; }
+    public decimal LivingOffSavingAmount { get; private set; }
+    public MonthlySaving LivingOffSaving { get; init; } = default!;
+    public decimal Balance => Amount - LivingOffSavingAmount;
+
+    public SavingResult(MonthlySaving monthlyLivingOff, string name, decimal amount)
+    {
+        if (monthlyLivingOff is null || monthlyLivingOff.Amount > 0)
+        {
+            throw new ArgumentException("Living off saving amount must be less than or equal to 0.");
+        }
+
+        if (amount < monthlyLivingOff.Amount)
+        {
+            throw new ArgumentException("Amount must be greater than or equal to living off saving amount.");
+        }
+
+        Name = name;
+        Amount = amount;
+        LivingOffSaving = monthlyLivingOff;
+        LivingOffSavingAmount = -1 * monthlyLivingOff.Amount;
+    }
+
+    public SavingResult(DateTime date, string name, decimal amount)
+    {
+        Name = name;
+        Amount = amount;
+        LivingOffSaving = new MonthlySaving() { Date = date, Name = name };
+    }
+
+    public void SetLivingOffSavingAmount(decimal amount)
+    {
+        if (amount < 0) return;
+        LivingOffSavingAmount = amount;
+        LivingOffSaving.Amount = -1 * amount;
+    }
 }
