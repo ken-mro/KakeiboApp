31627c06e66163bb84dac1d4abdf8c0d482fc626
Ken Murao <murao.ken.36x@kyoto-u.jp>
2025-01-03 12:00:01 +0900

    Add class to share between pages and bind it

---
+++ Models/SharedProperty.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace KakeiboApp.Models;
+
+public partial class SharedProperty : ObservableObject
+{
+    private static readonly SharedProperty _instance = new SharedProperty();
+    public static SharedProperty Instance => _instance;
+    [ObservableProperty]
+    DateTime _selectedDate = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
+}
