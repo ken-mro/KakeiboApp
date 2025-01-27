c6fa75ee38c2ed8ecf24dcc5738cb0f79ab8e624
Ken Murao <murao.ken.36x@kyoto-u.jp>
2025-01-27 23:47:28 +0900

    Enable to react to the apptheme changes

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,11 +4,20 @@ namespace KakeiboApp;
 
 public partial class App : Application
 {
-    public App(AppShell appShell)
+    public App(AppShell appShell, SettingPreferences settingPreferences)
     {
         Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(Constants.SYNCFUSIOHN_LICENSE_KEY);
         InitializeComponent();
 
         MainPage = appShell;
+
+
+        settingPreferences.SetAppTheme();
+
+        if (Current is null) return;
+        Current.RequestedThemeChanged += (s, a) =>
+        {
+            settingPreferences.SetAppTheme();
+        };
     }
 }
