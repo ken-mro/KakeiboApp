05cbb7d9cd4b1336e995c8308d1d6aa12d10975e
Ken Murao <murao.ken.36x@kyoto-u.jp>
2025-01-27 23:47:59 +0900

    Enable to set Theme on setting page.

--- ViewModels/SettingPageViewModel.cs
+++ ViewModels/SettingPageViewModel.cs
@@ -1,10 +1,18 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using KakeiboApp.Repository;
+using System.Collections.ObjectModel;
 
 namespace KakeiboApp.ViewModels;
 
 public partial class SettingPageViewModel : BaseViewModel
 {
+    private Dictionary<string, AppTheme> _themDictionary = new Dictionary<string, AppTheme>
+    {
+        { "システム", AppTheme.Unspecified },
+        { "ライト", AppTheme.Light },
+        { "ダーク", AppTheme.Dark }
+    };
+
     private readonly SettingPreferences _settingPreferences;
     public SettingPageViewModel(SettingPreferences settingPreferences)
     {
@@ -20,4 +28,17 @@ public partial class SettingPageViewModel : BaseViewModel
             OnPropertyChanged();
         }
     }
+
+    public string SelectedTheme
+    {
+        get => _themDictionary.FirstOrDefault(x => x.Value.ToString().Equals(_settingPreferences.Theme)).Key;
+        set
+        {
+            _settingPreferences.Theme = _themDictionary[value].ToString();
+            OnPropertyChanged();
+        }
+    }
+
+    [ObservableProperty]
+    ObservableCollection<string> _themes = ["システム", "ライト", "ダーク"];
 }
