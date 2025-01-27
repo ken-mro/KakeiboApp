790a1d42b7bc1c16b0f0181ef3ed3bd98bc996c1
Ken Murao <murao.ken.36x@kyoto-u.jp>
2025-01-27 23:45:15 +0900

    Add to be able to save AppTheme

--- Repository/SettingPreferences.cs
+++ Repository/SettingPreferences.cs
@@ -3,6 +3,13 @@ namespace KakeiboApp.Repository;
 public class SettingPreferences
 {
 	private IPreferences _defaultPreferences;
+    private Dictionary<string, AppTheme> _themDictionary = new Dictionary<string, AppTheme>
+    {
+        { AppTheme.Unspecified.ToString(), AppTheme.Unspecified },
+        { AppTheme.Light.ToString(), AppTheme.Light },
+        { AppTheme.Dark.ToString(), AppTheme.Dark }
+    };
+
     public SettingPreferences(IPreferences preferences)
 	{
         _defaultPreferences = preferences;
@@ -14,5 +21,26 @@ public class SettingPreferences
     {
         get => _defaultPreferences.Get(_isFingerprintEnabledKey, false);
         set => _defaultPreferences.Set(_isFingerprintEnabledKey, value);
+    }
+
+    private string _themeKey = "AppTheme";
+    private string _defaultTheme = AppTheme.Unspecified.ToString();
+
+    // Using string instead of AppTheme because IPreferences does not support enum.
+    public string Theme
+    {
+        get => _defaultPreferences.Get(_themeKey, _defaultTheme);
+        set
+        {
+            _defaultPreferences.Set(_themeKey, value);
+            if (App.Current is null) return;
+            App.Current.UserAppTheme = _themDictionary[value];
+        }
+    }
+
+    public void SetAppTheme()
+    {
+        if (App.Current is null) return;
+        App.Current.UserAppTheme = _themDictionary[Theme];
     }
 }
